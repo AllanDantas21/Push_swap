38f8b30211fa59411e2d91e81b67c232a18bc206
AllanDantas21 <Allandantas21.ad@gmail.com>
2023-12-18 15:37:23 -0300

    add push_swap and moves files

--- push_swap.c
+++ push_swap.c
@@ -1,24 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aldantas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/18 13:03:22 by aldantas          #+#    #+#             */
-/*   Updated: 2023/12/18 15:34:07 by aldantas         ###   ########.fr       */
+/*   Created: 2023/12/18 15:32:54 by aldantas          #+#    #+#             */
+/*   Updated: 2023/12/18 15:33:26 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef PUSH_SWAP.H
-# define PUSH_SWAP.H
-
-# include "../libft/libft.h"
-
-typedef struct s_list
-{
-	int		value;
-	int		pos;
-	struct s_list	*next;
-}	t_list;
-
-#endif
+#include "push_swap.h"
