14ab83b28b87e6dd8ff8c8c252e8dc9311a36e5d
AllanDantas21 <Allandantas21.ad@gmail.com>
2023-12-18 13:25:11 -0300

    create struct

--- push_swap.h
+++ push_swap.h
@@ -6,11 +6,17 @@
 /*   By: aldantas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 13:03:22 by aldantas          #+#    #+#             */
-/*   Updated: 2023/12/18 13:09:06 by aldantas         ###   ########.fr       */
+/*   Updated: 2023/12/18 13:17:51 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PUSH_SWAP.H
 # define PUSH_SWAP.H
 
+typedef struct s_list
+{
+	int		value;
+	int		pos;
+	struct s_list	*next;
+}	t_list;
 
 #endif
