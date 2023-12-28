9708a6f0a8667e5b85678d216c03301f91625c33
AllanDantas21 <Allandantas21.ad@gmail.com>
2023-12-28 17:39:51 -0300

    add sort_four in sort_small -> make more tests

--- src/sort_small.c
+++ src/sort_small.c
@@ -7,6 +7,8 @@ static void	sort_three(t_list **stack_a)
 
 	max = get_max_index(stack_a);
 	head_a = *stack_a;
+	if (is_sorted(stack_a))
+		return ;
 	if ((head_a->index == max))
 	{
 		ra(stack_a);
@@ -24,6 +26,32 @@ static void	sort_three(t_list **stack_a)
 		sa(stack_a);
 }
 
+static void	sort_four(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*head_a;
+
+	head_a = *stack_a;
+	while (head_a->index != 0)
+	{
+		ra(stack_a);
+		head_a = *stack_a;
+	}
+	if (head_a->index == 0)
+	{
+		pb(stack_a, stack_b);
+		sort_three(stack_a);
+		pa(stack_a, stack_b);
+		return ;
+	}
+}
+
+static void	sort_five(t_list **stack_a, t_list **stack_b)
+{
+	(void)stack_a;
+ 	(void)stack_b;
+	return ;
+}
+
 void	sort_small(t_list **stack_a, t_list **stack_b)
 {
  	(void)stack_a;
@@ -35,4 +63,8 @@ void	sort_small(t_list **stack_a, t_list **stack_b)
 		sa(stack_a);
 	if (size_lst == 3)
 		sort_three(stack_a);
+	if (size_lst == 4)
+		sort_four(stack_a, stack_b);
+	if (size_lst == 5)
+		sort_five(stack_a, stack_b);
 }
