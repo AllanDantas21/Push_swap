8afe8a973cc802baeca50bcda9d1e6da0a8ae2b5
AllanDantas21 <Allandantas21.ad@gmail.com>
2023-12-28 17:57:05 -0300

    refact sort_four function

--- src/sort_small.c
+++ src/sort_small.c
@@ -28,20 +28,13 @@ static void	sort_three(t_list **stack_a)
 
 static void	sort_four(t_list **stack_a, t_list **stack_b)
 {
-	t_list	*head_a;
-
-	head_a = *stack_a;
-	if (head_a->next->next->next->index == 0)
-	{
+	if (is_sorted(stack_a))
+		return ;
+	if ((*stack_a)->next->next->next->index == 0)
 		rra(stack_a);
-		head_a = *stack_a;
-	}
-	while (head_a->index != 0)
-	{
+	while ((*stack_a)->index != 0)
 		ra(stack_a);
-		head_a = *stack_a;
-	}
-	if (head_a->index == 0)
+	if ((*stack_a)->index == 0)
 	{
 		pb(stack_a, stack_b);
 		sort_three(stack_a);
@@ -59,8 +52,6 @@ static void	sort_five(t_list **stack_a, t_list **stack_b)
 
 void	sort_small(t_list **stack_a, t_list **stack_b)
 {
- 	(void)stack_a;
- 	(void)stack_b;
 	int	size_lst;
 	
 	size_lst = ft_lstsize(*stack_a);
