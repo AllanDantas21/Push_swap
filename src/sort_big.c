86a1facab09ce704ad2a4e34cae725357eff2618
AllanDantas21 <Allandantas21.ad@gmail.com>
2023-12-29 16:53:52 -0300

    improve sort_big with sort_stack_b algorithm

--- src/sort_big.c
+++ src/sort_big.c
@@ -18,7 +18,6 @@ static int	get_max_bits(t_list **stack)
 {
 	int		max_bits;
 	int		max;
-
 	max = get_max_index(stack);
 	max_bits = 0;
 	while ((max >> max_bits) != 0)
@@ -26,28 +25,42 @@ static int	get_max_bits(t_list **stack)
 	return (max_bits);
 }
 
-void	sort_big(t_list **stack_a, t_list **stack_b)
+static void	sort_stack_b(t_list **stack_a, t_list **stack_b, int size, int max_bits, int j)
 {
-	int		i;
-	int		j;
-	int		size;
-	int		max_bits;
-
-	i = 0;
-	size = ft_lstsize(*stack_a);
-	max_bits = get_max_bits(stack_a);
-	while (i < max_bits)
+	while (size-- && j <= max_bits && !is_sorted(stack_a))
 	{
-		j = 0;
-		while (j++ < size)
-		{
-			if ((((*stack_a)->index >> i) & 1) == 1)
-				ra(stack_a);
-			else
-				pb(stack_a, stack_b);
-		}
+		if ((((*stack_b)->index >> j) & 1) == 0)
+			ra(stack_b);
+		else
+			pa(stack_a, stack_b);
+	}
+	if (is_sorted(stack_a))
 		while (ft_lstsize(*stack_b) != 0)
 			pa(stack_a, stack_b);
-		i++;
+}
+
+void	sort_big(t_list **stack_a, t_list **stack_b)
+{
+	int	j;
+	int	max_bits;
+	int	size;
+
+	max_bits = get_max_bits(stack_a);
+	size =ft_lstsize(*stack_a);
+	j = 0;
+	while (j <= max_bits)
+	{
+		size = ft_lstsize(*stack_a);
+		while (size-- && !is_sorted(stack_a))
+		{
+			if ((((*stack_a)->index >> j) & 1) == 0)
+				pb(stack_a, stack_b);
+			else
+				ra(stack_a);
+		}
+		sort_stack_b(stack_a, stack_b, ft_lstsize(*stack_b), max_bits, j + 1);
+		j++;
 	}
+	while (ft_lstsize(*stack_b) != 0)
+		pa(stack_a, stack_b);
 }
