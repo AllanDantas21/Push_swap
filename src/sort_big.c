3ec8a50cf82486fe48014a104f85b0e965bf2bab
AllanDantas21 <Allandantas21.ad@gmail.com>
2023-12-29 16:56:33 -0300

    change condition sort_big

--- src/sort_big.c
+++ src/sort_big.c
@@ -29,10 +29,10 @@ static void	sort_stack_b(t_list **stack_a, t_list **stack_b, int size, int max_b
 {
 	while (size-- && j <= max_bits && !is_sorted(stack_a))
 	{
-		if ((((*stack_b)->index >> j) & 1) == 0)
-			ra(stack_b);
-		else
+		if ((((*stack_b)->index >> j) & 1) == 1)
 			pa(stack_a, stack_b);
+		else
+			ra(stack_b);
 	}
 	if (is_sorted(stack_a))
 		while (ft_lstsize(*stack_b) != 0)
@@ -53,10 +53,10 @@ void	sort_big(t_list **stack_a, t_list **stack_b)
 		size = ft_lstsize(*stack_a);
 		while (size-- && !is_sorted(stack_a))
 		{
-			if ((((*stack_a)->index >> j) & 1) == 0)
-				pb(stack_a, stack_b);
-			else
+			if ((((*stack_a)->index >> j) & 1) == 1)
 				ra(stack_a);
+			else
+				pb(stack_a, stack_b);
 		}
 		sort_stack_b(stack_a, stack_b, ft_lstsize(*stack_b), max_bits, j + 1);
 		j++;
