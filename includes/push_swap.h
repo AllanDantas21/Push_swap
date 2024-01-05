1c2cc3af3dc404e3f752ce162423b53bdc1742d9
AllanDantas21 <Allandantas21.ad@gmail.com>
2024-01-05 00:29:13 -0300

    remove main funcs moves in ;h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -34,11 +34,6 @@ void	ft_error(char *msg);
 void	*ft_free(char **mat);
 
 // Movimentos da push_swap
-int		rotate_stack(t_list **stack);
-int		push_stack(t_list **stack_dst, t_list **stack_src);
-int		swap_stack(t_list **stack);
-int		rev_rotate_stack(t_list **stack);
-
 int		sa(t_list **stack_a);
 int		sb(t_list **stack_b);
 int		ss(t_list **stack_a, t_list **stack_b);
