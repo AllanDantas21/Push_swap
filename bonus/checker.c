528a139d35e1e722d5ba77e84a1efc96ebabfb68
AllanDantas21 <Allandantas21.ad@gmail.com>
2024-01-13 23:31:33 -0300

    refact bonus main

--- bonus/checker.c
+++ bonus/checker.c
@@ -1,5 +1,22 @@
 #include "../includes/checker.h"
 
+static t_list **setup_stack(void)
+{
+	t_list	**tmp;
+
+	tmp = (t_list **)malloc(sizeof(t_list));
+	if(!tmp)
+		return (NULL);
+	*tmp = NULL;
+	return (tmp);
+}
+
+static void	free_stacks(t_list **stack_a, t_list **stack_b)
+{
+	free_stack(stack_a);
+	free_stack(stack_b);
+}
+
 int main(int argc, char **argv)
 {
 	t_list	**stack_a;
@@ -8,11 +25,9 @@ int main(int argc, char **argv)
 
 	if (argc < 2)
 		return (0);
-	stack_a = (t_list **)malloc(sizeof(t_list));
-	stack_b = (t_list **)malloc(sizeof(t_list));
-	*stack_a = NULL;
-	*stack_b = NULL;
 	check_args(argv, argc);
+	stack_a = setup_stack();
+	stack_b = setup_stack();
 	init_stack(stack_a, argc, argv);
 	line = get_next_line(0);
 	if (!line && !is_sorted(stack_a))
@@ -21,7 +36,6 @@ int main(int argc, char **argv)
 		write(1, "OK\n", 3);
 	else
 		ft_checker(line, stack_a, stack_b);
-	free_stack(stack_a);
-	free_stack(stack_b);
+	free_stacks(stack_a, stack_b);
 	return (0);
 }
