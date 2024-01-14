e1a8599efee57e8492a77b62ce45150be70975b3
AllanDantas21 <Allandantas21.ad@gmail.com>
2024-01-14 01:06:05 -0300

    refact validations.c -> checker

--- bonus/validations.c
+++ bonus/validations.c
@@ -3,20 +3,13 @@
 int swaps(char *line, t_list **stack_a, t_list **stack_b)
 {
 	if (line[0] == 's' && line[1] == 'a' && line[2] == '\n')
-	{
 		swap_stack(stack_a);
-		return (0);
-	}
 	else if (line[0] == 's' && line[1] == 'b' && line[2] == '\n')
-	{
 		swap_stack(stack_b);
-		return (0);
-	}
 	else if (line[0] == 's' && line[1] == 's' && line[2] == '\n')
 	{
 		swap_stack(stack_a);
 		swap_stack(stack_b);
-		return (0);
 	}
 	else
 	{
@@ -29,15 +22,9 @@ int swaps(char *line, t_list **stack_a, t_list **stack_b)
 int push(char *line, t_list **stack_a, t_list **stack_b)
 {
 	if (line[0] == 'p' && line[1] == 'a' && line[2] == '\n')
-	{
 		push_stack(stack_a, stack_b);
-		return (0);
-	}
-	else if (line[0] == 'p' && line[1] == 'b' && line[2] == '\n')
-	{
+	else if (line[0] == 'p' && line[1] == 'b' && line[2] == '\n')	
 		push_stack(stack_b, stack_a);
-		return (0);
-	}
 	else
 	{
 		write(2, "Error\n", 6);
@@ -49,15 +36,9 @@ int push(char *line, t_list **stack_a, t_list **stack_b)
 int	rotates(char *line, t_list **stack_a, t_list **stack_b)
 {
 	 if (line[0] == 'r' && line[1] == 'a' && line[2] == '\n')
-	{
 		rotate_stack(stack_a);
-		return (0);
-	}
 	else if (line[0] == 'r' && line[1] == 'b' && line[2] == '\n')
-	{
 		rotate_stack(stack_b);
-		return (0);
-	}
 	else if (line[0] == 'r' && line[1] == 'r' && line[3] == '\n')
 	{
 		rotate_stack(stack_a);
@@ -75,15 +56,9 @@ int	rotates(char *line, t_list **stack_a, t_list **stack_b)
 int	rev_rotates(char *line, t_list **stack_a, t_list **stack_b)
 {
 	if (line[0] == 'r' && line[1] == 'r' && line[2] == 'a' && line[3] == '\n')
-	{
 		rev_rotate_stack(stack_a);
-		return (0);
-	}
 	else if (line[0] == 'r' && line[1] == 'r' && line[2] == 'b' && line[3] == '\n')
-	{
 		rev_rotate_stack(stack_b);
-		return (0);
-	}
 	else if (line[0] == 'r' && line[1] == 'r' && line[2] == 'r' && line[3] == '\n')
 	{
 		rev_rotate_stack(stack_a);
